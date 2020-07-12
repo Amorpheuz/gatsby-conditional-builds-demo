ca93e036a715b58b9b89e127a4d6cded7cb9a53c
Amorpheuz <mail2ypd@gmail.com>
2020-07-12 21:23:36 +0530

    complete signing guestbook instructions

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -11,7 +11,7 @@ export default function Bio() {
       <Text>
         Hey everyone <span role="img" aria-label="Waves hand">👋🏽</span>, welcome to the Guestbook of <Link href='https://amorpheuz.dev/'>Yash Dave</Link>!
         I built this site to test out <Link href="https://www.gatsbyjs.org/docs/conditional-page-builds/">Gatsby Conditional Page Builds</Link> using <Link href="https://github.com/features/actions">GitHub Actions</Link> <span role="img" aria-label="A Robot automating things">🤖</span>.
-        While you are here, Feel free to <span role="img" aria-label="Signing hand">✍🏽</span> <Link sx={{variant: 'link.highlight'}} href="#">Sign the Guestbook</Link> to see a live demo of how the action works!
+        While you are here, Feel free to <span role="img" aria-label="Signing hand">✍🏽</span> <Link sx={{variant: 'link.highlight'}} href="./sign-the-guestbook/">Sign the Guestbook</Link> to see a live demo of how the action works!
       </Text>
     </Box>
   )
